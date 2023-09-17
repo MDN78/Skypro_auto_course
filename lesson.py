7bdaf8c0cef2535be1bda06a72275282972360a7
Dmitry <dmitry-maksimov@yandex.ru>
2023-09-17 16:30:26 +0300

    add file gitignore

--- lesson.py
+++ lesson.py
@@ -1,25 +1,5 @@
-# get rating
-rate = int(input("Add your rating from 1 - 5 "))
+# file for training
 
-# check rating from 1 til 5
-if rate < 1:
-    rate = 1
-
-
-if rate > 5:
-    rate = 5
-
-feedback = ''
-
-if rate == 1:
-    feedback = input(" Express your opinion ")
-elif rate == 2:
-    feedback = input("WHF fellow!?! ")
-elif rate == 3:
-    feedback = input("so-so, let's check your problem ")
-else:
-    print("OK")
-    
-print(rate)
-print(feedback)
+for i in range(1, 21):
+    print(f"X = {i}, and X2 = {i ** 2}")
     
