d6dad50af3d41b00a6f5312192ddad8bb4978a05
Dmitry <dmitry-maksimov@yandex.ru>
2023-09-17 15:00:49 +0300

    update task

--- lesson_1/lesson_1_task_8.py
+++ lesson_1/lesson_1_task_8.py
@@ -1,6 +1,15 @@
 
 def print_number(num):
-    return num
+    print(num)
 
-print(print_number(8), print_number(8), print_number(0), print_number(0), print_number(0), print_number(5), print_number(5), 
-      print_number(5), print_number(3), print_number(5), print_number(3), print_number(5))
+print_number(8)
+print_number(8)
+print_number(0)
+print_number(0)
+print_number(5)
+print_number(5)
+print_number(5)
+print_number(3)
+print_number(5)
+print_number(3)
+print_number(5)
