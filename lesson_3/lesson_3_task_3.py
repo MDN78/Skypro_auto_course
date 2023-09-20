0cf2f88db40c8be64745868bd05ee458a267bd71
Dmitry <dmitry-maksimov@yandex.ru>
2023-09-20 21:04:13 +0300

    rebuild file lesson_e_task_3

--- lesson_3/lesson_3_task_3.py
+++ lesson_3/lesson_3_task_3.py
@@ -1,8 +1,11 @@
 from mailing import Mailing
+from address import Address
 
-to_send = Mailing("197373", "Moscow", "Lenina", "123", "23")
-from_send = Mailing("188888", "Pskov", "Petrova", "23", "78")
+to_send = Address("197373", "Moscow", "Lenina", "123", "23")
+from_send = Address("188888", "Pskov", "Petrova", "23", "78")
 
-print(f"Отправление {Mailing.track('1')} из {from_send.to_address()} в {to_send.from_address()}. Стоимость {Mailing.cost(23500)} рублей")
+delivery = Mailing("123", "22222")
+
+print(f"Отправление {delivery.cost} из {delivery.from_address(from_send.address())} в {delivery.to_address(to_send.address())}. Стоимость {delivery.cost} рублей.")
 
 
--- lesson_3/mailing.py
+++ lesson_3/mailing.py
@@ -1,21 +1,21 @@
-from address import Address
 
-class Mailing(Address):
+class Mailing():
     
-    def cost(cost):
-        return cost
+    def __init__(self, cost, track):
+        self.cost = cost
+        self.track = track
+            
+    def cost(self):
+        return self.cost
     
-    def track(track):
-        return track
+    def track(self):
+        return self.track
         
-    def to_address(self):
-        to_address = Address.address(self)
+    def to_address(self, to_address):
         return to_address
         
-    def from_address(self):
-        from_address = Address.address(self)
+    def from_address(sel, from_address):
         return from_address
-    
     
 
 
