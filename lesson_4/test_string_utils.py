34dbbb5ed31759e0466d7be16079341b55e01f54
Dmitry <dmitry-maksimov@yandex.ru>
2023-09-23 21:51:28 +0300

    update homework

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -10,7 +10,8 @@ utils = StringUtils()
     ('sKyPrO', 'Skypro'),
     ('скайпро', 'Скайпро'),
     ('  skypro', 'skypro'),
-    ('  второй', 'второй')
+    ('  второй', 'второй'),
+    (' Sky Pro', 'Sky Pro')
     ])
 def test_capitalize_and_remove_empty(date, result):
     utils = StringUtils()
@@ -25,8 +26,14 @@ def test_capitalize_and_remove_empty(date, result):
 
 @pytest.mark.parametrize('string, symbol', [
     ('SkyPro', 'S'),
-    ('Proverka', 'a')
-    ])      
+    ('Proverka', 'a'),
+    ('Second', 's'),
+    ('sos', 's'),
+    ('Проверка', 'П'),
+    ('Вторая', 'я'),
+    ('1234', '1')
+    ])
+  
 def test_start_or_end_with(string, symbol):
     utils = StringUtils()
     if string[0] == symbol:
@@ -35,4 +42,32 @@ def test_start_or_end_with(string, symbol):
         assert res == True
     elif string[-1] == symbol:
         print("Start testing function 'end_with")
+        res = utils.end_with(string, symbol)
+        assert res == True
+
+
+@pytest.mark.parametrize('string, symbol',[
+    ('SkyPro', 'S'),
+    ('Привет', 'р')
+    ])       
+def test_contains_positive(string, symbol):
+    utils = StringUtils()
+    res = utils.contains(string, symbol)
+    assert res == True
+    
+@pytest.mark.parametrize('string, symbol', [
+    ('SkyPro', 's'),
+    ('Привет', 'я')
+    ])       
+def test_contains_negative(string, symbol):
+    utils = StringUtils()
+    res = utils.contains(string, symbol)
+    assert res == False
+ 
+
+@pytest.mark.xfail(strict=True)
+def test_to_list_positive(string, delimiter, result):
+    utils = StringUtils()
+    res = utils.to_list(string, delimiter)
+    assert res == result
     
